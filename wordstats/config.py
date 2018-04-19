5df18393b5ba2ad3ff28a1bd36e1a5d812255284
joel <joel@sfgr.nl>
2018-04-19 16:43:46 +0200

    path global parameters added

--- wordstats/config.py
+++ wordstats/config.py
@@ -6,5 +6,10 @@ if db_uri.startswith("mysql"):
     db_uri += '?charset=utf8'
 
 DATA_FOLDER= 'language_data/hermitdave/2016'
+DATA_FOLDER_COGNATES= 'language_data/cognates'
+BLACKLIST= 'blacklist'
+WHITELIST= 'whitelist'
+RULES= 'rules'
+CANDIDATES= 'candidates'
 
-MAX_WORDS = 10000
+MAX_WORDS = 1000
