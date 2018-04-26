e0e22f4a6f28073b48faedcbb110a65c92ef124c
joel <joel@sfgr.nl>
2018-04-26 13:22:47 +0200

    renamed abstract class

--- wordstats/__init__.py
+++ wordstats/__init__.py
@@ -8,7 +8,7 @@ from .word_info import WordInfo
 from .language_info import LanguageInfo
 from .cognate_info import CognateInfo
 from .getchunix import _Getch
-from .word_distance import WordDistance
+from .edit_distance_function_factory import WordDistanceFactory
 
 # Create all tables in the engine. equivalent to "Create Table" in SQL
 Base.metadata.create_all(BaseService.engine)
