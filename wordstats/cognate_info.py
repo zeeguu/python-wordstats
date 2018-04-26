f4900a5c5f35744025a2e921ff8335850b19b58a
joel <joel@sfgr.nl>
2018-04-26 13:24:39 +0200

    moved from list to set, database support added, moved several functions elsewhere

--- wordstats/cognate_info.py
+++ wordstats/cognate_info.py
@@ -10,29 +10,31 @@ from .base_service import BaseService, Base
 from .config import MAX_WORDS
 from .metrics_computers import *
 from .cognate_files_path import *
+from .cognate_db import *
 from .getchunix import read_single_keypress
 
 
 class CognateInfo(object):
     def __init__(self, language_ids, method_name):
         self.language_ids = language_ids
-        self.whitelist = []
-        self.candidates = []
-        self.blacklist = []
-        self.rules = []
+        self.whitelist = set()
+        self.candidates = set()
+        self.blacklist = set()
         self.method_name = method_name
 
     def all_blacklist(self):
-        return self.blacklist.items()
+        return self.blacklist
 
     def all_whitelist(self):
-        return self.whitelist.items()
+        return self.whitelist
 
-    def all_rules(self):
-        return self.rules.items()
+    def add_to_whitelist(self, cognate):
+        self.whitelist.add(cognate)
+        append_to_file(path_of_cognate_whitelist(self.language_ids), cognate)
 
-    def __getitem__(self, key):
-        return self.get(key)
+    def add_to_blacklist(self, cognate):
+        self.blacklist.add(cognate)
+        append_to_file(path_of_cognate_blacklist(self.language_ids), cognate)
 
 
     # also save new dicts
@@ -58,13 +60,13 @@ class CognateInfo(object):
         new_registry = cls(language_ids, method_name)
 
         language_code_path = path_of_cognate_whitelist(language_ids)
-        new_registry.whitelist = load_from_path(language_code_path).splitlines()
+        new_registry.whitelist = set(load_from_path(language_code_path).splitlines())
 
         language_code_path = path_of_cognate_blacklist(language_ids)
-        new_registry.blacklist = load_from_path(language_code_path).splitlines()
+        new_registry.blacklist = set(load_from_path(language_code_path).splitlines())
 
         language_code_path = path_of_cognate_candidates(language_ids, method_name)
-        new_registry.candidates = load_from_path(language_code_path).splitlines()
+        new_registry.candidates = set(load_from_path(language_code_path).splitlines())
 
         return new_registry
 
@@ -85,50 +87,38 @@ class CognateInfo(object):
     def apply_distance_metric(self, wordlist1, wordlist2, func):
 
         for w1 in wordlist1:
-            newlist = list(filter(lambda x: len(x)>0 , map(lambda w: w1 + ' ' + w if func(w1, w) else "", wordlist2)))
-            self.candidates.extend(newlist)
-
-    # note: only executable in terminal, reactive to one-key stroke
-    def start_quiz(self):
-        print(self.language_ids)
-        print("y:   whitelist")
-        print("q:   quit")
-        print("other:   blacklist")
-
-        for cognate in self.candidates:
-            print(cognate)
-
-            char = read_single_keypress()
-
-            if char == 'y':
-                self.whitelist.append(cognate)
-            elif char == 'q':
-                break
-            else:
-                self.blacklist.append(cognate)
-
-
-
+            newset = set(filter(lambda x: len(x)>0 , map(lambda w: w1 + ' ' + w if func(w1, w) else "", wordlist2)))
+            self.candidates = self.candidates.union(newset)
 
     # TODO: implement load and cache from /to db
     @classmethod
-    def load_from_db(cls, cognate_code, method_name= ""):
+    def load_from_db(cls, language_ids, method_name= ""):
         """
         Assumes the ~./word_info file contains
         information about how to connect to the
         database. Also, assumes the database contains
         the information.
 
-        :param language_id:
+        :param language_ids:
         :return:
         """
 
-        new_registry = cls(cognate_code, method_name= "")
+        new_registry = cls(language_ids, method_name= "")
 
-        all_word_info_items = WordInfo.find_all(cognate_code)
+        all_word_info_items = CognateCandidatesInfo.find_all(language_ids, method_name)
 
         for each in all_word_info_items:
-            new_registry.word_info_dict[each.word] = each
+            word = each.word_from + " " + each.word_to
+            new_registry.candidates.add(word)
+
+        all_word_info_items = CognateWhiteListInfo.find_all(language_ids)
+
+        for each in all_word_info_items:
+            word = each.word_from + " " + each.word_to
+            if each.whitelist:
+                new_registry.whitelist.add(word)
+            else:
+                new_registry.blacklist.add(word)
 
         return new_registry
 
@@ -140,28 +130,44 @@ class CognateInfo(object):
         :return:
         """
 
-        def clear_corresponding_entries_in_db(self):
-            table = Table('word_info', Base.metadata, autoload=True, autoload_with=BaseService.engine)
-            words = BaseService.session.query(table).filter(table.c.language_id == self.language_id)
+        def clear_corresponding_entries_in_db_candidates(self):
+            table = Table('candidates_info', Base.metadata, autoload=True, autoload_with=BaseService.engine)
+            words = BaseService.session.query(table).filter(table.c.language_ids == self.language_ids)\
+                .filter(table.c.method == self.method_name)
             words.delete(synchronize_session=False)
 
-        clear_corresponding_entries_in_db(self)
-        for word_info in list(self.word_info_dict.values()):
-            BaseService.session.add(word_info)
+        def clear_corresponding_entries_in_db_whitelist(self):
+            table = Table('cognate_whitelist_info', Base.metadata, autoload=True, autoload_with=BaseService.engine)
+            words = BaseService.session.query(table).filter(table.c.language_ids == self.language_ids)
+            words.delete(synchronize_session=False)
+
+        clear_corresponding_entries_in_db_candidates(self)
+        for word_info in self.candidates:
+            BaseService.session.add(CognateCandidatesInfo(word_info.split()[0], word_info.split()[1],
+                                                  self.language_ids,self.method_name))
+
+        clear_corresponding_entries_in_db_whitelist(self)
+        for word_info in self.whitelist:
+            BaseService.session.add(CognateWhiteListInfo(word_info.split()[0], word_info.split()[1],
+                                                  self.language_ids,True))
+
+        for word_info in self.blacklist:
+            BaseService.session.add(CognateWhiteListInfo(word_info.split()[0], word_info.split()[1],
+                                                  self.language_ids,False))
+
         BaseService.session.commit()
 
     # Everything that follows is private
-    def print_load_stats(self, a, b):
-        memory_footprint = total_size(self.blacklist, set()) / 1024 / 1024
-        print(("Elapsed time to load the {0} data: {1} ({2} entries)".format(self.language_ids, b - a,
-                                                                             len(self.blacklist))))
-        print(("Required memory for the {0} registry: {1}MB".format(self.language_ids, memory_footprint)))
-
+    def _print_load_stats(self, a, b):
+        memory_footprint = total_size(self.candidates, set()) / 1024 / 1024
+        print(("Elapsed time to load the {0} data: {1} ({2} entries)".format(self.candidates, b - a,
+                                                                             len(self.candidates))))
+        print(("Required memory for the {0} {1} registry: {2}MB".format(self.language_ids, self.method_name, memory_footprint)))
     @classmethod
-    def profile_load_from_db(cls, language_id, output=False):
+    def _pprofile_load_from_db(cls, language_ids, method_name, output=False):
 
         a = datetime.now()
-        new_registry = cls.load_from_db(language_id)
+        new_registry = cls.load_from_db(language_ids, method_name)
         b = datetime.now()
         if output:
             new_registry.print_load_stats(a, b)
@@ -169,10 +175,10 @@ class CognateInfo(object):
         return b - a
 
     @classmethod
-    def profile_load_from_file(cls, cognate_code, method_name= "", output=False):
+    def _pprofile_load_from_file(cls, language_ids, method_name= "", output=False):
 
         a = datetime.now()
-        new_registry = cls.load_from_file(cognate_code, method_name= "")
+        new_registry = cls.load_from_file(language_ids, method_name= "")
         b = datetime.now()
         if output:
             new_registry.print_load_stats(a, b)
