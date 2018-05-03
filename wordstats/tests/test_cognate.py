e790041c72c9b2d3cf43659025779a53a7243447
joel <joel@sfgr.nl>
2018-05-03 14:38:58 +0200

    additional test added

--- wordstats/tests/test_cognate.py
+++ wordstats/tests/test_cognate.py
@@ -22,30 +22,41 @@ class CognateTests(TestCase):
 
         #cognate_info_de_nl = CognateInfo.load_cached("de", "nl", LanguageAwareEditDistance)
 
-        cognate_info_de_fr = CognateInfo.load_cached("de", "nl", LanguageAwareEditDistance)
+        cognate_info_de_nl = CognateInfo.load_cached("de", "nl", LanguageAwareEditDistance)
 
 #        assert (len(cognate_info_de_nl.best_guess()) <= len(cognate_info_de_nl.candidates))
 
         #assert (cognate_info_de_nl.has_cognates("als"))
         #assert (cognate_info_de_nl.get_cognates("als"))
 
-        cognate_info_de_fr.save_candidates()
+        cognate_info_de_nl.save_candidates()
         # save the 
         # - config 
         # - candidates
 
 
     @classmethod
-    def testLoadCached(self):
-        cognate_info_de_fr = CognateInfo("fr", "nl", LanguageAwareEditDistance)
-        cognate_info_de_fr.compute()
-        cognate_info_de_fr.save_candidates()
-        cognate_info_de_fr.cache_to_db()
+    def testManualCompute(self):
+        cognate_info_fr_nl = CognateInfo("fr", "nl", LanguageAwareEditDistance)
+        cognate_info_fr_nl.compute()
+
+        cognate_info_fr_nl.save_candidates()
+        cognate_info_fr_nl.save_evaluation()
+        cognate_info_fr_nl.cache_to_db()
 
     @classmethod
-    def testoverlap(self):
-        cognate_info_de_fr = CognateInfo("de", "nl", WordDistanceOverlap)
-        cognate_info_de_fr.compute()
-        cognate_info_de_fr.cache_to_db()
-        cognate_info_de_fr.save_candidates()
-        print(cognate_info_de_fr.candidates.items())
+    def testOverlap(self):
+        cognate_info_de_nl = CognateInfo.load_cached("de", "nl", WordDistanceOverlap)
+
+        cognate_info_de_nl.cache_to_db()
+        cognate_info_de_nl.save_candidates()
+        cognate_info_de_nl.save_evaluation()
+        print(cognate_info_de_nl.candidates.items())
+
+    @classmethod
+    def testRetrieveCognates(self):
+        cognate_info_de_nl = CognateInfo.load_cached("de", "nl", WordDistanceOverlap)
+
+        for primaryWord in cognate_info_de_nl.candidates.keys():
+            print(primaryWord, cognate_info_de_nl.get_cognates(primaryWord))
+            assert(cognate_info_de_nl.has_cognates(primaryWord))
