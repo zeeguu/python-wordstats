632b1b015b2cbe0dbc84a0ca1885ec3b4d7e385f
Joël <j.h.l.grondman@student.rug.nl>
2018-07-01 19:33:44 +0200

    converted to python3 style

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 from wordstats import Word
 
 
-print Word.stats("maman", "fr")
-print Word.stats("Mutter", "de")
-print Word.stats("Mother", "en")
-print Word.stats("mama", "ro")
+print(str(Word.stats("maman", "fr")))
+print(str(Word.stats("Mutter", "de")))
+print(str(Word.stats("Mother", "en")))
+print(str(Word.stats("mama", "ro")))
 
-print Word.stats("maman", "fr")
-print Word.stats("Mutter", "de")
-print Word.stats("Mother", "en")
-print Word.stats("mamamama", "ro").importance
+print(str(Word.stats("maman", "fr")))
+print(str(Word.stats("Mutter", "de")))
+print(str(Word.stats("Mother", "en")))
+print(str(Word.stats("mamamama", "ro").importance))
