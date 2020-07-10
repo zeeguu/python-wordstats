48534a7b87851737c40544ab46c636232ac558f4
Mircea Lungu <mircea.lungu@gmail.com>
2020-07-10 12:54:06 +0300

    Update example.py

--- example.py
+++ example.py
@@ -3,32 +3,31 @@ from wordstats.common_words import common_words
 
 French = LanguageInfo.load("fr")
 
-
 print("Top 10 most used words in French")
 print(French.all_words()[:10])
 print(" ")
-
 
 print("Info about the word 'jamais'")
 jamais_info = French.get("jamais")
 print(jamais_info)
 print(" ")
 
+print("Word info via the Word class")
+from wordstats import Word
+print(Word.stats('bleu', 'fr'))
 
-print("First 10 common words of more than 10 letters common between French and English")
-
+print("First four common words of more than 10 letters common between French and English")
 English = LanguageInfo.load("en")
 count = 0
 for each in French.all_words():
     if len(each) > 10 and each in English.all_words():
         print(each)
         count += 1
-    if count == 10:
+    if count == 4:
         break
 print("")
 
-
-print("Words (>8chars) common in all the languages*")
+print("Words (>9chars) common in all the languages*")
 for each in common_words():
-    if len(each) > 8:
+    if len(each) > 9:
         print(each)
