695906ffd86f155187cbef9961983e7c6b8a79ba
Mircea Lungu <mircea.lungu@gmail.com>
2020-07-10 13:12:24 +0300

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from wordstats.common_words import common_words
 
 French = LanguageInfo.load("fr")
 
-print("Top 10 most used words in French")
+print("Top 10 most used words in Dutch")
 print(French.all_words()[:10])
 print(" ")
 
@@ -17,12 +17,14 @@ from wordstats import Word
 print(Word.stats('bleu', 'fr'))
 
 print("First four common words of more than 10 letters common between French and English")
-English = LanguageInfo.load("en")
+Polish = LanguageInfo.load("pl")
+Romanian = LanguageInfo.load("ro")
 count = 0
-for each in French.all_words():
-    if len(each) > 10 and each in English.all_words():
-        print(each)
-        count += 1
+for each in Polish.all_words():
+    if each in Romanian.all_words():
+        if len(each) > 5 and each not in common_words():
+            print(each)
+            count += 1
     if count == 4:
         break
 print("")
@@ -31,3 +33,8 @@ print("Words (>9chars) common in all the languages*")
 for each in common_words():
     if len(each) > 9:
         print(each)
+print("")
+
+
+print ("Is blauzungekrankenheit more difficult than blau?")
+print (Word.stats('blauzungekrankenheit','de').difficulty > Word.stats('blau','de').difficulty)
